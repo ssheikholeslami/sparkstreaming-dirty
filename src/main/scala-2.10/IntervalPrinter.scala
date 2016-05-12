98cb7909fe8e9f480798e2d21d84e82ef1d8b8e5
Sina Sheikholeslami <ssheikholeslami@gmail.com>
2016-05-12 12:29:12 +0430

    IntervalPrinter

---
+++ src/main/scala-2.10/IntervalPrinter.scala
@@ -0,0 +1,55 @@
+/**
+  * Created by sinash on 5/9/16.
+  *
+  * A simple Spark Streaming program that prints and counts stream input elements in intervals of 10 seconds
+  * To use it, run netcat on port 9999: nc -lk 9999
+  */
+
+
+import org.apache.spark._
+import org.apache.spark.streaming._
+import org.apache.log4j.Logger
+import org.apache.log4j.Level
+
+
+
+object IntervalPrinter {
+
+  var intervalCount = 1 // FIXME should use accumulators! , cluster mode consideration
+  var elementCount = 0 // FIXME should use accumulators! , cluster mode consideration
+
+  def main(args: Array[String]) {
+
+    Logger.getLogger("org").setLevel(Level.OFF) //disable logging
+
+
+    // 2 cores for master to prevent starvation
+    val conf = new SparkConf().setMaster("local[2]").setAppName("StreamingOne")
+    val ssc = new StreamingContext(conf, Seconds(10)) // batch interval of 10 seconds
+
+    //DStream receiving from localhost:9999
+
+    val lines = ssc.socketTextStream("localhost", 9999)
+
+    lines.foreachRDD{ (rdd, time) =>
+
+      println("Batch " + intervalCount + " contents: ")
+      rdd.foreach(element => {
+        println(element)
+        elementCount = elementCount + 1
+      })
+
+      intervalCount = intervalCount + 1
+      println("element count: " + elementCount)
+
+    }
+    // to start the processing
+
+    ssc.start()
+    ssc.awaitTermination() // wait for the computation to terminate
+
+  }
+
+
+
+}
