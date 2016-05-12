f92e4f8ea31eab28d2ef4e9b79f61cd539c12e09
Sina Sheikholeslami <ssheikholeslami@gmail.com>
2016-05-12 14:04:41 +0430

    A Random Number Generator + A Interval Aggregator (summation) Spark Streaming App

---
+++ src/main/scala-2.10/RandomNumberGenerator.scala
@@ -0,0 +1,44 @@
+import java.io.PrintWriter
+import java.net.ServerSocket
+
+import scala.util.Random
+
+/**
+  * Created by sinash on 5/12/16.
+  * Using an example code from "Machine Learning with Spark" book by Nick Pentreath, Packt Publishing
+  */
+object RandomNumberGenerator {
+
+
+  def main(args: Array[String]) {
+    val random = new Random()
+//    random.setSeed(1)
+    val maxEventsPerSecond = 5
+
+    //create a network producer
+    val listener = new ServerSocket(9999)
+    println("listening on 9999")
+
+    while(true){
+      val socket = listener.accept()
+
+      new Thread(){
+        override def run = {
+          println("client connected from: " + socket.getInetAddress)
+          val out = new PrintWriter(socket.getOutputStream(), true)
+
+          while(true){
+            Thread.sleep(1000)
+            val num = random.nextInt(10)
+            out.print(num)
+            out.write("\n")
+            out.flush()
+          }
+          socket.close()
+        }
+
+      }.start()
+
+    }
+  }
+}
