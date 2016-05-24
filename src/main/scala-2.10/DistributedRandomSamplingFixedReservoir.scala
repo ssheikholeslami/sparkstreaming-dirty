9fe40fbd09286b69f67e356d56bff8987c858b18
Sina Sheikholeslami <ssheikholeslami@gmail.com>
2016-05-25 00:50:07 +0430

    [MAJOR] [TESTED] RandomNumberGenerator and Distributed... should now be launched with runtime arguments

--- src/main/scala-2.10/DistributedRandomSamplingFixedReservoir.scala
+++ src/main/scala-2.10/DistributedRandomSamplingFixedReservoir.scala
@@ -2,40 +2,42 @@
   * Created by sinash on 5/14/16.
   * Distributed version of "Random Sampling With a Fixed Reservoir (Vitter)"
   */
-import org.apache.spark._
-import org.apache.spark.streaming._
-import org.apache.log4j.Logger
-import org.apache.log4j.Level
+
 import java.util.Random
 
+import org.apache.log4j.{Level, Logger}
+import org.apache.spark._
+import org.apache.spark.streaming._
+
 object DistributedRandomSamplingFixedReservoir {
-  //FIXME overflow may happen, if the stream is very big!
-  var globalCount: Long = 1
-  var reservoirSize = 10 //FIXME runtime argument!
-  var sampleSet = new Array[Long](reservoirSize)
 
   def main(args: Array[String]) {
+
+    if (args.length < 4) {
+      System.err.println("Usage: <source hostname> <source port> <interval duration> <reservoir size>")
+      System.exit(1)
+    }
+
+    val reservoirSize = args(3).toInt
+    var sampleSet = new Array[Long](reservoirSize)
 
     Logger.getLogger("org").setLevel(Level.OFF) //disable logging
 
     // at least 2 cores to prevent starvation (1 core for each stream source, and at least one worker)
-
-    //FIXME set cores from runtime arguments
-    //FIXME set master address from runtime arguments
-//    val conf = new SparkConf().setMaster("local[2]").setAppName("DistributedRandomSampling")
-
+    //    val conf = new SparkConf().setMaster("local[2]").setAppName("DistributedRandomSampling")
     val conf = new SparkConf().setAppName("DistributedRandomSampling")
-    val ssc = new StreamingContext(conf, Seconds(10)) // batch interval duration FIXME set from runtime arguments
+    val ssc = new StreamingContext(conf, Seconds(args(2).toLong)) // batch interval duration FIXME set from runtime arguments
 
 
     //DStream receiving from localhost:9999
-    //FIXME set input source via SDMiner GUI
 
-    val stream = ssc.socketTextStream("localhost", 9999)
-    //FIXME zipWithIndex or zipWithUniqueId ?
-    val indexedDStream = stream.transform(rdd => rdd.zipWithUniqueId().mapValues( id => id + globalCount))
+    val stream = ssc.socketTextStream(args(0), args(1).toInt)
+    val indexedDStream = stream.transform(rdd => rdd.map(element => {
+      val splitted = element.split(" ")
+      (splitted(0), splitted(1).toLong)
+    }))
 
-    val filteredDStream = indexedDStream.filter(element => (reservoirSize.toFloat/((element._2).toFloat)) > (new Random()).nextFloat())
+    val filteredDStream = indexedDStream.filter(element => (reservoirSize.toFloat / ((element._2).toFloat)) > (new Random()).nextFloat())
 
     filteredDStream.foreachRDD(rdd => {
       val updateSet = rdd.collect()
@@ -43,16 +45,14 @@ object DistributedRandomSamplingFixedReservoir {
       //update the sample
       sortedUpdateSet.foreach(element => {
 
-        val replaceIndex = (new Random()).nextInt(10)
+        val replaceIndex = (new Random()).nextInt(reservoirSize)
         sampleSet.update(replaceIndex, (element._1).toLong)
 
-        println(element)
-        println("UPDATED")
+        println("ToBeIncluded: " + element)
       })
 
-      println("Current Sample: " + sampleSet.mkString(" "))    })
-
-    indexedDStream.foreachRDD{(rdd => globalCount += rdd.count())} //is it safe to use indexedDStream again?
+      println("Current Sample: " + sampleSet.mkString(" "))
+    })
 
     // to start the processing
     ssc.start()
--- src/main/scala-2.10/RandomNumberGenerator.scala
+++ src/main/scala-2.10/RandomNumberGenerator.scala
@@ -5,32 +5,41 @@ import scala.util.Random
 
 /**
   * Created by sinash on 5/12/16.
-  * Using an example code from "Machine Learning with Spark" book by Nick Pentreath, Packt Publishing
+  * UBased on example code from "Machine Learning with Spark" book by Nick Pentreath, Packt Publishing
   */
 object RandomNumberGenerator {
-
+  var counter = 1
 
   def main(args: Array[String]) {
     val random = new Random()
-//    random.setSeed(1)
-    val maxEventsPerSecond = 5
+
+    if (args.length < 3) {
+      System.err.println("Usage: <port> <max value> <events per second>")
+      System.exit(1)
+    }
 
     //create a network producer
-    val listener = new ServerSocket(9999)
+    val listener = new ServerSocket(args(0).toInt)
     println("listening on 9999")
-
-    while(true){
+    var sleepTime = 100 //default, 10 events per second
+    if (args(2).toInt != 0) {
+      sleepTime = 1000 / args(2).toInt
+    }
+    while (true) {
       val socket = listener.accept()
-
-      new Thread(){
+      counter = 1
+      new Thread() {
         override def run = {
           println("client connected from: " + socket.getInetAddress)
           val out = new PrintWriter(socket.getOutputStream(), true)
 
-          while(true){
-            Thread.sleep(1)
-            val num = random.nextInt(10)
-            out.print(num)
+          while (true) {
+
+            Thread.sleep(sleepTime)
+            val num = random.nextInt(args(1).toInt)
+            out.print(num + " " + counter)
+//            println(num + " " + counter) //for debug
+            counter = counter + 1
             out.write("\n")
             out.flush()
           }
